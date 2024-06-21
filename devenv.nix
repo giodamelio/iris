19e15cb984db931611c188e7b918bb4da38985c1
Giovanni d'Amelio <gio@damelio.net>
2024-06-21 13:01:13 -0700

    Install a newer version of Surrealist from a PR

--- devenv.nix
+++ devenv.nix
@@ -1,12 +1,15 @@
-{ pkgs, lib, config, inputs, ... }:
-
-{
+{ pkgs, lib, config, inputs, ... }: let
+  system = pkgs.stdenv.system;
+in {
   languages.rust = {
     enable = true;
     channel = "nightly";
   };
 
   packages = with pkgs; [
-    surrealist
+    surrealdb
+
+    # TODO: remove this when a new version lands in Nixpkgs unstable
+    inputs.nixpkgs-surrealist-pr.legacyPackages.${system}.surrealist
   ];
 }
