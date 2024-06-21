ef8abe9df7ca49eb18f5cf6a604017eadceaa9ae
Giovanni d'Amelio <gio@damelio.net>
2024-06-21 16:05:52 -0700

    Meta tags are important

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,8 @@ fn layout(m: Markup) -> Markup {
         (maud::DOCTYPE)
         head {
             meta charset="utf-8";
+            meta name="viewport" content="width=device-width, initial-scale=1";
+            meta name="color-scheme" content="light dark";
 
             link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css";
         }
