b7d21eaa6f8f1a5896f5cef5b64a9bfc36bdde7c
Giovanni d'Amelio <gio@damelio.net>
2024-06-29 11:57:07 -0700

    Setup basic CookieSession with a new key generate every startup

--- src/routes.rs
+++ src/routes.rs
@@ -1,30 +1,45 @@
 use anyhow::Result;
 use maud::html;
-use poem::session::{CookieConfig, MemoryStorage, ServerSession};
+use poem::session::{CookieConfig, CookieSession, Session};
+use poem::web::cookie::CookieKey;
 use poem::{get, handler, EndpointExt, Route};
+use tokio::sync::OnceCell;
 
+use crate::db::DB;
 use crate::template::Template;
 use crate::views::layout;
 
-pub fn routes() -> impl EndpointExt {
+// Get or generate our cookie key
+static COOKIE_KEY: OnceCell<CookieKey> = OnceCell::const_new();
+pub async fn cookie_key(db: &DB) -> &CookieKey {
+    COOKIE_KEY
+        .get_or_init(|| async { CookieKey::generate() })
+        .await
+}
+
+pub async fn routes(db: &DB) -> impl EndpointExt {
+    let cookie_key = cookie_key(db).await;
+
     Route::new()
         .at("/", get(index))
-        .with(ServerSession::<MemoryStorage>::new(
-            CookieConfig::new(),
-            MemoryStorage::new(),
-        ))
+        .with(CookieSession::new(CookieConfig::private(
+            cookie_key.clone(),
+        )))
 }
 
 #[handler]
-async fn index() -> Result<Template> {
+async fn index(session: &Session) -> Result<Template> {
+    let count = session.get::<i32>("count").unwrap_or(0) + 1;
+    session.set("count", count);
+
     let response = html! {
         article {
             header {
                 strong {
-                    "HELLO"
+                    "HELLO WORLD"
                 }
             }
-            "WORLD"
+            "You have been here " (count) " times!"
         }
     };
 
