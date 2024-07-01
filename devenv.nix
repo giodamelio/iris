a67d4d4fbe289236dc007b64e2f9524d4a48de93
Giovanni d'Amelio <gio@damelio.net>
2024-07-01 13:15:29 -0700

    Add cargo watch script for auto reloading server

--- devenv.nix
+++ devenv.nix
@@ -8,6 +8,7 @@ in {
 
   packages = with pkgs; [
     surrealdb
+    cargo-watch
 
     # TODO: remove this when a new version lands in Nixpkgs unstable
     inputs.nixpkgs-surrealist-pr.legacyPackages.${system}.surrealist
@@ -16,4 +17,8 @@ in {
   processes.surrealdb = {
     exec = "${pkgs.surrealdb}/bin/surreal start --bind 127.0.0.1:8000 file:dev.db";
   };
+
+  scripts.dev.exec = "cargo watch -x run";
+
+  env.RUST_LOG = "iris=trace";
 }
