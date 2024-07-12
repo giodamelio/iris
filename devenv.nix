9c986b413c2381a44b896d0add5c9aca4b3b391d
Giovanni d'Amelio <gio@damelio.net>
2024-07-12 15:58:25 -0700

    Make Elixir Language Server startup properly in my Neovim

--- devenv.nix
+++ devenv.nix
@@ -5,6 +5,8 @@
     inotify-tools
   ];
 
+  env.ELIXIRLS_CMD = "${pkgs.elixir-ls}/lib/language_server.sh";
+
   enterShell = ''
     export HEX_HOME="$DEVENV_ROOT/.hex";
     export MIX_HOME="$DEVENV_ROOT/.mix";
