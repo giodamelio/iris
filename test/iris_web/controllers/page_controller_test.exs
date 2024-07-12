3e71a86deb35db6fba1670810fafe86692f9ad90
Giovanni d'Amelio <gio@damelio.net>
2024-07-12 14:47:22 -0700

    Clean up the homepage

--- test/iris_web/controllers/page_controller_test.exs
+++ test/iris_web/controllers/page_controller_test.exs
@@ -3,6 +3,7 @@ defmodule IrisWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Register"
+    assert html_response(conn, 200) =~ "Log In"
   end
 end
