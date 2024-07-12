ae7bebcaf9c2c8e6f9e3e09198c4a3a7f1d3ffbb
Giovanni d'Amelio <gio@damelio.net>
2024-07-12 14:26:26 -0700

    Make Phoenix Autoreloading Work

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,10 @@
 { pkgs, lib, config, inputs, ... }: {
   languages.elixir.enable = true;
 
+  packages = with pkgs; [
+    inotify-tools
+  ];
+
   enterShell = ''
     export HEX_HOME="$DEVENV_ROOT/.hex";
     export MIX_HOME="$DEVENV_ROOT/.mix";
