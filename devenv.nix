d53e3b014b468fda152ad073a7b946d0282b7227
Giovanni d'Amelio <gio@damelio.net>
2024-07-12 14:09:07 -0700

    Save the Hex/Mix deps locally in the current directory

--- devenv.nix
+++ devenv.nix
@@ -1,3 +1,8 @@
 { pkgs, lib, config, inputs, ... }: {
   languages.elixir.enable = true;
+
+  enterShell = ''
+    export HEX_HOME="$DEVENV_ROOT/.hex";
+    export MIX_HOME="$DEVENV_ROOT/.mix";
+  '';
 }
