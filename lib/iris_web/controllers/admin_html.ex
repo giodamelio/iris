e309e92f2cea521dd7d38bea38d10629638a5a5c
Giovanni d'Amelio <gio@damelio.net>
2024-07-27 08:29:11 -0700

    Render a actual link to the invite for easy testing

--- lib/iris_web/controllers/admin_html.ex
+++ lib/iris_web/controllers/admin_html.ex
@@ -1,5 +1,11 @@
 defmodule IrisWeb.AdminHTML do
   use IrisWeb, :html
 
+  alias Iris.Accounts.UserInvite
+
   embed_templates "admin_html/*"
+
+  def generate_invite_url(%UserInvite{external_id: external_id}) do
+    url(~p"/invite/user/#{external_id}")
+  end
 end
