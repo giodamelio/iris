19a5287232b67828967c98c8c2caa7927d4ed8bd
Giovanni d'Amelio <gio@damelio.net>
2024-07-28 09:33:18 -0700

    Fix mix format

--- lib/iris/application.ex
+++ lib/iris/application.ex
@@ -11,8 +11,7 @@ defmodule Iris.Application do
       IrisWeb.Telemetry,
       Iris.Repo,
       {Ecto.Migrator,
-        repos: Application.fetch_env!(:iris, :ecto_repos),
-        skip: skip_migrations?()},
+       repos: Application.fetch_env!(:iris, :ecto_repos), skip: skip_migrations?()},
       {DNSCluster, query: Application.get_env(:iris, :dns_cluster_query) || :ignore},
       {Phoenix.PubSub, name: Iris.PubSub},
       # Start the Finch HTTP client for sending emails
