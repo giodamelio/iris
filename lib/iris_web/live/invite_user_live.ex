d596d2aa33da7774818ee5a2711aca8181628f24
Giovanni d'Amelio <gio@damelio.net>
2024-07-30 13:19:55 -0700

    Update user invite page to use it's own form not the generated one

--- lib/iris_web/live/invite_user_live.ex
+++ lib/iris_web/live/invite_user_live.ex
@@ -17,26 +17,48 @@ defmodule IrisWeb.InviteUserLive do
   @impl true
   def render(assigns) do
     ~H"""
-    <.live_component
-      module={IrisWeb.UserLive.FormComponent}
-      id={:new_from_invite}
-      title="Create User"
-      action={:new_from_invite}
-      user={%User{}}
-      invite={assigns.invite}
-    />
+    <.section title="Create User">
+      <.simple_form for={@form} id="user-form" phx-change="validate" phx-submit="save">
+        <.input field={@form[:full_name]} type="text" label="Full name" />
+        <.input field={@form[:email]} type="text" label="Email" />
+        <:actions>
+          <.button phx-disable-with="Saving..." class="button is-primary">Save User</.button>
+        </:actions>
+      </.simple_form>
+    </.section>
     """
   end
 
   @impl true
   def mount(%{"id" => external_id} = _params, _session, socket) do
     invite = Accounts.get_user_invite_by_external_id(external_id)
-    socket = assign(socket, :invite, invite)
+
+    socket =
+      socket
+      |> assign(:invite, invite)
+      |> assign(:form, to_form(Accounts.change_user(%User{})))
+
     {:ok, socket}
   end
 
   @impl true
-  def handle_info({IrisWeb.UserLive.FormComponent, {:saved, _user}}, socket) do
-    {:noreply, push_navigate(socket, to: "/")}
+  def handle_event("validate", %{"user" => user_params}, socket) do
+    form =
+      %User{}
+      |> Accounts.change_user(user_params)
+      |> to_form(action: :validate)
+
+    {:noreply, assign(socket, form: form)}
+  end
+
+  @impl true
+  def handle_event("save", %{"user" => user_params}, socket) do
+    case Accounts.create_user_from_invite(user_params, socket.assigns.invite) do
+      {:ok, _user} ->
+        {:noreply, redirect(socket, to: ~p"/")}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, form: to_form(changeset))}
+    end
   end
 end
---
+++ test/iris_web/live/invite_user_live_test.exs
@@ -0,0 +1,24 @@
+defmodule IrisWeb.InviteUserLiveTest do
+  use IrisWeb.ConnCase, async: true
+
+  import Phoenix.LiveViewTest
+  import Iris.AccountsFixtures
+  alias Iris.Accounts
+
+  @create_attrs %{email: "some email", full_name: "some full_name"}
+
+  test "creates a user successfully", %{conn: conn} do
+    invite = user_invite_fixture()
+
+    {:ok, invite_live, _html} = live(conn, ~p"/invite/user/#{invite.external_id}")
+
+    {:ok, conn} =
+      invite_live
+      |> form("#user-form", user: @create_attrs)
+      |> render_submit()
+      |> follow_redirect(conn, ~p"/")
+
+    assert Accounts.count_users() == 1
+    assert html_response(conn, 200) =~ "Hello World"
+  end
+end
